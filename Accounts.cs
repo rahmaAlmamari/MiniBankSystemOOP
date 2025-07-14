a0fca96adc31038be9e2d2de409f562959671770
rahmaAlmamari <alr816125@gmail.com>
2025-07-14 15:32:08 +0400

    Adding feilds to accounts class

--- Accounts.cs
+++ Accounts.cs
@@ -8,5 +8,16 @@ namespace MiniBankSystemOOP
 {
     class Accounts
     {
+        //1. Account class feilds ...
+        private int Number = 0;
+        public string UserName;
+        private string NationalID;
+        private double Balance;
+        public string PhoneNumber;
+        public string Address;
+        private string Password;
+        public bool IsActive; // active = true or 1, inactive = false or 0
+        public string Type = "EndUser";
+
     }
 }
