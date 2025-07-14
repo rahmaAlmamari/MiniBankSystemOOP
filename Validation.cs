eb34e78010924c2fe564912ba3d643bbf9102be5
rahmaAlmamari <alr816125@gmail.com>
2025-07-14 14:54:10 +0400

    Adding CharValidation method to Validation class

--- Validation.cs
+++ Validation.cs
@@ -9,6 +9,29 @@ namespace MiniBankSystemOOP
     //This class is used to validate user input ...
     class Validation
     {
+        //1. CharValidation method ...
+        public static char CharValidation(string message)
+        {
+            bool CharFlag;//to handle user char error input ...
+            char CharInput = '0';
+            do
+            {
+                try
+                {
+                    CharFlag = false;
+                    Console.WriteLine($"Enter your {message}:");
+                    CharInput = char.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Your {message} not accepted due to: " + e.Message);
+                    CharFlag = true;
+                }
 
+            } while (CharFlag);
+
+            //to return tne char input ...
+            return CharInput;
+        }
     }
 }
