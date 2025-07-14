37775cecae42d1b60074c951f614de07e553c281
rahmaAlmamari <alr816125@gmail.com>
2025-07-14 14:46:40 +0400

    create validation class which will contain all input validation

---
+++ Validation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBankSystemOOP
+{
+    //This class is used to validate user input ...
+    class Validation
+    {
+
+    }
+}
