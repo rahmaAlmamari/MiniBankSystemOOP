f82f8a2c83cea8f67bcc353013131c9eee2ac3f7
rahmaAlmamari <alr816125@gmail.com>
2025-07-14 15:07:20 +0400

    Modify the main method basa

--- Program.cs
+++ Program.cs
@@ -6,6 +6,42 @@
         {
             //to call WelcomeMessage method ...
             Additional.WelcomeMessage();
+            //to keep the system runs until user choose to closed the system ...
+            bool MainRun = true;//to stop main method ...
+            while (MainRun)
+            {
+                Console.Clear();//to clear the screen ...
+                Console.WriteLine("1. Sing up");
+                Console.WriteLine("2. Log in");
+                Console.WriteLine("0. Log out");
+                //to call CharValidation to get and validate user input ...
+                char MainOption = Validation.CharValidation("option");
+                //to run the option user want ...
+                switch (MainOption)
+                {
+                    case '1'://to call SingIn method ...
+                        //SingUp();
+                        Console.WriteLine("Sing up method is not implemented yet.");
+                        break;
+
+                    case '2'://to call LogIn method ...
+                        //LogIn();
+                        Console.WriteLine("Log in method is not implemented yet.");
+                        break;
+
+                    case '0'://to log out Main ...
+                        //to display exit message ...
+                        Console.WriteLine("Have a nice day (^0^)");
+                        MainRun = false;//to stop the while loop ...
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        Additional.HoldScreen();//to hold the screen ...
+                        break;
+                }
+
+            }
         }
     }
 }
