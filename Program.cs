3f368ba7d94364aac7cae3cd6d06a0eed037b898
rahmaAlmamari <alr816125@gmail.com>
2025-07-14 14:30:55 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniBankSystemOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
