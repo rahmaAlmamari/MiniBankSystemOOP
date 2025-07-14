6ca171b7131f453acca6154e74a2916ecd63366c
rahmaAlmamari <alr816125@gmail.com>
2025-07-14 14:53:03 +0400

    Adding HoldScreen method to Additional class

--- Additional.cs
+++ Additional.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace MiniBankSystemOOP
 {
+    //This class is used to hold additional methods that are used in the system ...
     class Additional
     {
+        //1. To hoad the screen ...
+        public static void HoldScreen()
+        {
+            Console.WriteLine("Press (Enter Kay) to continue");
+            Console.ReadLine();
+        }
     }
 }
