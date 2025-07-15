874831a140ab68bf55bb353808bf1b7551921a0a
rahmaAlmamari <alr816125@gmail.com>
2025-07-15 11:35:53 +0400

    Creating EndUserMenu method and call it in LogIn method

--- Accounts.cs
+++ Accounts.cs
@@ -52,13 +52,10 @@ namespace MiniBankSystemOOP
                    Program.UserAccounts[i].P_Password == inputPassword)
                 {
                     //to check if the account is active ...
-                    if (!Program.UserAccounts[i].IsActive && Program.UserAccounts[i].Type == "EndUser")
+                    if (Program.UserAccounts[i].IsActive && Program.UserAccounts[i].Type == "EndUser")
                     {
-                        //to display welcome message ...
-                        Console.WriteLine($"Welcome our deer end user {Program.UserAccounts[i].UserName} to Codeline Bank System");
-                        Additional.HoldScreen();//to hold the screen ...
                         //to call EndUserMenu method from Accounts class ...
-                        //EndUserMenu();
+                        EndUserMenu();
                     }
                     else if (Program.UserAccounts[i].IsActive && Program.UserAccounts[i].Type == "Admin")
                     {
@@ -78,6 +75,49 @@ namespace MiniBankSystemOOP
             }
 
         }
+        //3.2. EndUserMenu method ...
+        public static void EndUserMenu()
+        {
+            //to display EndUser menu options ...
+            Console.WriteLine("End User Menu:");
+            Console.WriteLine("1. View Account Details");
+            Console.WriteLine("2. Deposit Money");
+            Console.WriteLine("3. Withdraw Money");
+            Console.WriteLine("4. Transfer Money");
+            Console.WriteLine("5. Log Out");
+            //to get and validate user input for EndUser menu ...
+            char endUserOption = Validation.CharValidation("option");
+            //to run the option user want ...
+            switch (endUserOption)
+            {
+                case '1':
+                    //to call ViewAccountDetails method ...
+                    //ViewAccountDetails();
+                    break;
+                case '2':
+                    //to call DepositMoney method ...
+                    //DepositMoney();
+                    break;
+                case '3':
+                    //to call WithdrawMoney method ...
+                    //WithdrawMoney();
+                    break;
+                case '4':
+                    //to call TransferMoney method ...
+                    //TransferMoney();
+                    break;
+                case '5':
+                    //to log out EndUser ...
+                    Console.WriteLine("You have been logged out successfully.");
+                    Additional.HoldScreen();//to hold the screen ...
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    Additional.HoldScreen();//to hold the screen ...
+                    break;
+            }
+        }
+        //3.3. AdminMenu method ...
 
         //4. Account class constructors ...
         //4.1. Default constructor ...
