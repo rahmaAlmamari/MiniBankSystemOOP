4357b0f9fcd0c4113f1f92cea5675d149abd258c
rahmaAlmamari <alr816125@gmail.com>
2025-07-16 09:45:45 +0400

    Adding feilds to transction class

--- Transactions.cs
+++ Transactions.cs
@@ -8,6 +8,18 @@ namespace MiniBankSystemOOP
 {
     class Transactions
     {
+        //1. Transction class fields ...
+        public int TransactionID = 0;
+        public string TransactionType;
+        public double TransactionAmount;
+        public double TransactionBalance;//balance after transaction
+        public DateTime TransactionDate;
 
+        //2. Transaction class properties ...
+        //3. Transaction class constructor ...
+        public Transactions()
+        {
+            TransactionID++;
+        }
     }
 }
